19515d08e83174f1b135e4b9d90cd3ebe295281f
aKi-1201 <b10501015@ntu.edu.tw>
2025-10-14 22:32:47 +0800

    Initial Commit

---
+++ Spotify/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Spotify
+//
+//  Created by 114-1iosClassStudent05 on 2025/10/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Spotify/SpotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyApp.swift
+//  Spotify
+//
+//  Created by 114-1iosClassStudent05 on 2025/10/14.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
